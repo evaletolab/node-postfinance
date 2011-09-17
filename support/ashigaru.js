15b6c00b0ccbb65e8cd597838278e0ffac98b248
Branko Vukelic <branko@brankovukelic.com>
2011-09-17 12:33:37 +0200

    Updated to v0.0.9

--- support/ashigaru.js
+++ support/ashigaru.js
@@ -82,7 +82,7 @@
  * Ashigaru currently has no mechanisms for retrying a failed connection. You 
  * should make sure you handle such unforeseen problems yourself.
  *
- * @version 0.0.8
+ * @version 0.0.9
  * @author Branko Vurkelic <branko@herdhound.com>
  * @license MIT (see LICENSE)
  */
