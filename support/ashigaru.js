de48bd26e2f678044f77d23254f97217422bf679
Branko Vukelic <branko@brankovukelic.com>
2011-09-17 16:54:35 +0200

    Updated version number to 0.1.1

--- support/ashigaru.js
+++ support/ashigaru.js
@@ -82,7 +82,7 @@
  * Ashigaru currently has no mechanisms for retrying a failed connection. You 
  * should make sure you handle such unforeseen problems yourself.
  *
- * @version 0.1.0
+ * @version 0.1.1
  * @author Branko Vurkelic <branko@herdhound.com>
  * @license MIT (see LICENSE)
  */
