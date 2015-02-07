203da125ea433a2f4a450e914645834942dd2e86
evaletolab <evaleto@gmail.com>
2015-02-07 12:43:11 +0100

    utf8 small fix

--- test/postfinance.1.validate.sha.js
+++ test/postfinance.1.validate.sha.js
@@ -20,7 +20,7 @@ describe("postfinance.sha", function(){
 
   before(function(done){
     testSettings = require('../config-pf');
-    testSettings.debug = false; // Enables *blocking* debug output to STDOUT
+    testSettings.debug = true; // Enables *blocking* debug output to STDOUT
     config.reset()
     done()
   });
@@ -74,6 +74,28 @@ describe("postfinance.sha", function(){
     SHASIGN: 'B541784E4823892FEB14FB086906A07CD3632CC6D12302BAB9D0CE70276774CF' 
   }
 
+  var pspForm={
+    ALIAS:'1987839941297952',
+    ALIASUSAGE:'Karibou payment',
+    AMOUNT:'100',
+    BGCOLOR:'#F2F4F2',
+    CN:'oliviêr evalet',
+    CURRENCY:'CHF',
+    EMAIL:'evaleto@gmail.com',
+    LANGUAGE:'fr_FR',
+    OPERATION:'RES',
+    ORDERID:'AS1423307782163',
+    OWNERADDRESS:'route de chêne',
+    OWNERCITY:'Genève',
+    OWNERZIP:'1208',
+    PARAMPLUS:'createAlias=true&user=8772725',
+    PM:'postfinance card',
+    PSPID:'TEMPkaribou',
+    SHASIGN:'9D0201341C3D427015EF103A903A0CDD1B5246D99DFC558CFB44920EF35514D9',
+    TITLE:'Enregistrement de votre carte chez Postfinance',
+    TP:'http://api.karibou.evaletolab.ch/v1/psp/std'    
+  }
+
   var utf8hash={
     str:'ALIAS=19878399412979521testpostfinanceALIASUSAGE=Karibou payment1testpostfinanceAMOUNT=1001testpostfinanceBGCOLOR=#F2F4F21testpostfinanceCN=oliviêr evalet1testpostfinanceCURRENCY=CHF1testpostfinanceEMAIL=evaleto@gmail.com1testpostfinanceOPERATION=RES1testpostfinanceORDERID=AS14229784215701testpostfinanceOWNERZIP=12081testpostfinancePM=postfinance card1testpostfinancePSPID=TEMPkaribou1testpostfinanceTITLE=Enregistrement de votre carte chez Postfinance1testpostfinanceTP=http://localhost:4000/v1/psp/std1testpostfinance',
     hash:'24AD4DBE42F8AA52D4DC12B0A8AF7D03E761CD10951A63FAA8578D2A397ED692'
@@ -95,6 +117,13 @@ describe("postfinance.sha", function(){
   it("validate SHASIGN from psp/webhook  ", function(done){
     this.timeout(20000);
     postfinance.isValidSha(pspWebhook).should.be.true
+    done()
+
+  });
+
+  it("validate SHASIGN from psp Form ", function(done){
+    this.timeout(20000);
+    postfinance.isValidSha(pspForm).should.be.true
     done()
 
   });
